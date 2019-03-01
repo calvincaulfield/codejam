a6db4ad3b4afda92ec5aadb27839d70ce75cf954
南基雄 <kiun.namu@groove-x.com>
2019-03-01 16:18:50 +0900

    Solved second problem

--- src/solution/codejam_2008_q_B.py
+++ src/solution/codejam_2008_q_B.py
@@ -1,2 +1,52 @@
+
 def solve(input, output):
-    output.write(input.read())
+    def get_int():
+        return int(input.readline())
+
+    def get_string():
+        return input.readline()
+
+    def solve_case(num):
+        # All time units are minute
+        turn_around_time = get_int()
+        num_a_to_b, num_b_to_a = [int(x) for x in get_string().split(" ")]
+
+        # Departure time includes turnaround time
+        a_departures = []
+        a_arrivals = []
+        b_departures = []
+        b_arrivals = []
+        a_min = 0
+        b_min = 0
+
+        def process_timetable(num, departure_station, arrival_station):
+            for _ in range(num):
+                time_pair = [x for x in get_string().split(" ")]
+                departure, arrival = [60 * int(x.split(":")[0]) + int(x.split(":")[1]) for x in time_pair]
+                departure_station.append(departure)
+                arrival_station.append(arrival + turn_around_time)
+
+        def get_max_debt(departures, arrivals):
+            current = 0
+            minimum = 0
+            all_list = [(x, "departure") for x in departures] + [(x, "arrival") for x in arrivals]
+            all_list.sort()
+            for entry in all_list:
+                if (entry[1] == "departure"):
+                    current -= 1
+                    if (current < minimum):
+                        minimum = current
+                else:
+                    current += 1
+            return -minimum
+
+        process_timetable(num_a_to_b, a_departures, b_arrivals)
+        process_timetable(num_b_to_a, b_departures, a_arrivals)
+
+        #print(a_departures, a_arrivals)
+
+        output.write("Case #{}: {} {}\n".format(num, get_max_debt(a_departures, a_arrivals), get_max_debt(b_departures, b_arrivals)))
+
+    num_cases = get_int()
+    for i in range(num_cases):
+        solve_case(i + 1)
