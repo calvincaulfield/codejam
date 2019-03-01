65b845d78a3c2e33f1958742ebf3bef25115a7c7
南基雄 <kiun.namu@groove-x.com>
2019-03-01 14:20:11 +0900

    Solved first problem!

--- src/solution/codejam_2008_q_A.py
+++ src/solution/codejam_2008_q_A.py
@@ -1,2 +1,40 @@
+
 def solve(input, output):
-    output.write(input.read())
+    def get_int():
+        return int(input.readline())
+
+    def get_string():
+        return input.readline()
+
+    def solve_case(num):
+        servers = {}
+        num_used_server = 0
+        num_servers = get_int()
+        num_server_change = 0
+
+        for _ in range(num_servers):
+            servers[get_string()] = False
+        
+        num_search_keywords = get_int()
+        for _ in range(num_search_keywords):
+            keyword = get_string()
+            if (servers[keyword]):
+                pass
+            else:                
+                if (num_used_server + 1 == num_servers):
+                    # We need to change server now, before processing this keyword (though we don't know which one)
+                    num_server_change += 1
+
+                    # Start new cycle
+                    for x in servers:
+                        servers[x] = False
+                    num_used_server = 0
+
+                num_used_server += 1
+                servers[keyword] = True
+
+        output.write("Case #{}: {}\n".format(num, num_server_change))
+
+    num_cases = get_int()
+    for i in range(num_cases):
+        solve_case(i + 1)
