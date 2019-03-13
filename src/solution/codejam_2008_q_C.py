76c5c6e55036a36f3e374f9b287bd4edf50d75ca
南基雄 <kiun.namu@groove-x.com>
2019-03-13 18:16:18 +0900

    Solving in quite complex way (Euclidean geometry)

--- src/solution/codejam_2008_q_C.py
+++ src/solution/codejam_2008_q_C.py
@@ -8,6 +8,52 @@ def solve(input, output):
     def get_string():
         return input.readline().strip()
 
+    def square(x):
+        return x * x
+
+    def get_opposite_len(radius, x):
+        return math.sqrt(square(radius) - square(x))
+
+    def get_wrong_pie_area(radius, x):
+        # Return area right of vertical line
+        angle = math.acos(radius / x)
+        pie_area = square(radius) * angle / 2
+        y = get_opposite_len(radius, x)
+        return pie_area - (x * y / 2)
+
+    def get_triangle_area_of_circla(radius, x, y, square_size):
+        # x, y denotes bottom-left corner of gap square
+       
+        def is_strict_inside(x, y):
+            return square(x) + square(y) < square(radius)
+
+        def is_inclusive_inside(x, y):
+            return square(x) + square(y) <= square(radius)
+
+        if (not is_strict_inside(x, y)):
+            return 0
+        
+        elif (is_inclusive_inside(x + square_size, y + square_size)):
+            return square(square_size)
+
+        elif (is_strict_inside(x, y + square_size)):
+            new_x = get_opposite_len(radius, y + square_size)
+            offset_area = square_size * (new_x - x)
+            return get_triangle_area_of_circla(radius, new_x, y)
+
+        elif (is_strict_inside(x + square_size, y)):
+            return get_triangle_area_of_circla(radius, y, x, square_size)
+
+        else:
+            border_y = get_opposite_len(radius, x)
+
+            wrong_pie_small = get_wrong_pie_area(radius, border_y)
+            wrong_pie_large = get_wrong_pie_area(radius, y)
+
+            rectangle = x * (border_y - y)
+            
+            return wrong_pie_large - wrong_pie_small - rectangle)
+
     def solve_case():
         fly_r, racket_outer_r, racket_thickness, string_r, gap = [float(x) for x in get_string().split(" ")]
         racket_inner_r = racket_outer_r - racket_thickness
@@ -20,8 +66,10 @@ def solve(input, output):
 
         # We will calculate area of all gaps
         total_gap_area = 0
-
+        
+        # x, y denotes bottom-left corner of gap square
         for x in range(0, racket_inner_r, unit_square_size):
+            for y in range(0, racket_inner_r, unit_square_size):
 
 
         
